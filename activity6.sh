eb92c1b42a9be2d0bc38fbe4ac00fe94f17b6008
EC2 Default User <gracedean282@yahoo.com>
2022-04-14 15:17:29 +0000

    activity 6 and 4

--- activity6.sh
+++ activity6.sh
@@ -2,12 +2,31 @@
 ##################
 #activity 6 Grace Dean
 ##################
-a=10
-b=5
-if[$a -gt $b]
+echo "param 1: $1"
+echo "param 2: $2"
+
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+	echo "incorrect format"; exit 1
+fi
+
+re='^[0-9]+$'
+if ! [[ $2 =~ $re ]] ; then
+        echo "incorrect format"; exit 2
+
+fi
+if [ $1 -gt $2 ]
 then
-	echo "true"
+	echo "True"
 else
-	echo "false"
+	echo "False"
+fi
+if [ $1 -gt $2 ]
+then
+COUNT=$(($1 - $2))
+echo $COUNT
+else
+	COUNT=$(($2 - $1 ))
+	echo $COUNT
 fi
 
--- activitys4.sh
+++ activitys4.sh
@@ -2,6 +2,8 @@
 
 while true; do
 	read -r -p "Enter password: " VAR
+
+
 	if [[ $VAR =~ [A-Z] ]] 
 	then
 		echo "Contains uppercase"
@@ -9,10 +11,21 @@ while true; do
 		if [[ $VAR =~ [0-9] ]] 
 		then
 			echo "Contains numbers"
+			
+			length=$(expr length "$VAR")
+			if [[ length -gt 8 ]]
+			
+			then
+				echo "password meets length requirements"
+			else 
+			echo "password does not meet length requirements"
+			fi
 		fi
+		break
+
 	else
 	
-		echo "Does not contain numbers and letters"
+		echo "Does not meet requirements"
 		break
 	fi
 done
