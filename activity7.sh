a6bf5933ef58e270715ca7a63d7a18f8e0bd2c26
EC2 Default User <gracedean282@yahoo.com>
2022-04-19 15:10:32 +0000

    activity 3

--- activity7.sh
+++ activity7.sh
@@ -3,6 +3,24 @@
 #Activity 7 by Grace Dean
 ###############################
 
-while read LINE;
-do
-ssh 
+clear
+score=0
+while IFS='#' read -r question choices answer
+do 
+	echo
+	echo $question
+	echo
+	echo $choices
+	echo
+	echo "Your answer:\c"
+	read student_answer </dev/tty
+
+	if [ "$student_answer" = "$answer" ]; then
+		score='expr $score + 1'
+	fi
+	clear
+done <quiz.txt
+
+echo
+echo "Your score is: $score"
+echo
